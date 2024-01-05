9460b696aabea2899c3fe5846d7d2527d6bff00d
Cristiano Castiel <cristianoct03@gmail.com>
2024-01-04 21:08:23 -0300

    Seeding Service

--- SalesWebMvc02/Data/SalesWebMvc02Context.cs
+++ SalesWebMvc02/Data/SalesWebMvc02Context.cs
@@ -16,6 +16,6 @@ namespace SalesWebMvc02.Data
 
         public DbSet<Department> Department { get; set; }
         public DbSet <Seller> Seller { get; set; }
-        public DbSet<SalesRecord> salesRecord { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
