356a9f84e40d0db5d43b5f325cab05d3bf095f9c
Cristiano Castiel <cristianoct03@gmail.com>
2024-01-12 23:18:39 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc02/Controllers/SellersController.cs
+++ SalesWebMvc02/Controllers/SellersController.cs
@@ -5,16 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc02.Services;
 using SalesWebMvc02.Models;
+using SalesWebMvc02.Models.ViewModels;
 
 namespace SalesWebMvc02.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
@@ -24,7 +27,9 @@ namespace SalesWebMvc02.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ SalesWebMvc02/Services/DepartmentService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc02.Data;
+using SalesWebMvc02.Models;
+
+namespace SalesWebMvc02.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvc02Context _context;
+
+        public DepartmentService(SalesWebMvc02Context context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
