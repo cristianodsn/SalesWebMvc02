d6d981b966a293823f2c4f1b03100281c34a875d
Cristiano Castiel <cristianoct03@gmail.com>
2024-01-19 23:01:19 -0300

    Delete seller

--- SalesWebMvc02/Services/DepartmentService.cs
+++ SalesWebMvc02/Services/DepartmentService.cs
@@ -20,5 +20,17 @@ namespace SalesWebMvc02.Services
         {
             return _context.Department.OrderBy(x => x.Name).ToList();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
