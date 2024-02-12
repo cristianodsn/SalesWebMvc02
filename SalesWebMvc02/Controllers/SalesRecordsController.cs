f75d56e6b9ffc4d5dd8c468dc7ec7edbf9350197
Cristiano Castiel <cristianoct03@gmail.com>
2024-02-11 21:44:20 -0300

    Implementing grouping search

--- SalesWebMvc02/Controllers/SalesRecordsController.cs
+++ SalesWebMvc02/Controllers/SalesRecordsController.cs
@@ -37,9 +37,21 @@ namespace SalesWebMvc02.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 01, 01);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateGroupingAsinc(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- SalesWebMvc02/Services/SalesRecordService.cs
+++ SalesWebMvc02/Services/SalesRecordService.cs
@@ -36,5 +36,25 @@ namespace SalesWebMvc02.Services
                 .ToListAsync();
         }
 
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsinc (DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(obj => obj.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();               
+        }
+
     }
 }
